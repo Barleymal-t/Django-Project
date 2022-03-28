9aa292dcd722c570fc1e98c554139b2845532d13
Nii <niiodartey10@gmail.com>
2022-03-29 00:13:05 +0100

    app made using django and react

---
+++ helloWorld/reactapp/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className="center-column">
+
+      <div className="item-row"><span>
+        one
+      </span></div>
+      <div className="item-row"><span>
+        two
+      </span></div>
+      <div className="item-row"><span>
+        three
+      </span></div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
